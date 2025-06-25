1e460def44a61e96f53107d73bfe34f975ecf255
Hyperez-G <jmgperez27@gmail.com>
2025-06-25 16:13:33 +0800

    Fix the multiple alerts for email validation even if validation fails

--- js/form.js
+++ js/form.js
@@ -5,18 +5,16 @@ document.querySelector("form").addEventListener("submit", function(event) {
     var name = document.querySelector("[name='name']").value;
     var email = document.querySelector("[name='email']").value;
 
-    // Validate Name
     if (name === "") {
         alert("Name is required");
         return;
     }
 
-    // Validate Email
     if (email.indexOf('@') === -1) {
         alert("Invalid email");
         return;
     }
 
-    alert("Form submitted!");  
+    alert("Form submitted!");
     this.submit();
 });
