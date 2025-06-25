58521d810b57a68e65bd9d186ca264e7728327b8
jakethedog <22-78222@.batstate-u.edu.ph>
2025-06-25 15:42:52 +0800

    Remove the click handler and handle everything inside the submit event handler.

--- js/form.js
+++ js/form.js
@@ -1,19 +1,22 @@
 // Buggy form.js
 document.querySelector("form").addEventListener("submit", function(event) {
+event.preventDefault();
+
     var name = document.querySelector("[name='name']").value;
     var email = document.querySelector("[name='email']").value;
 
+    // Validate Name
+    if (name === "") {
+        alert("Name is required");
+        return;
+    }
+
+    // Validate Email
     if (email.indexOf('@') === -1) {
         alert("Invalid email");
+        return;
     }
 
     alert("Form submitted!");
-});
-
-var submitButton = document.querySelector("button");
-submitButton.addEventListener("click", function() {
-    var name = document.querySelector("[name='name']").value;
-    if (name === "") {
-        alert("Name is required");
-    }
+    this.submit();
 });
