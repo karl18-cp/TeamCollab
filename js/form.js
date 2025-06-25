46a7474f2c8547c3e2530aac504a296d9200ddd5
Hyperez-G <jmgperez27@gmail.com>
2025-06-25 16:06:02 +0800

    Added event.preventDefault() to prevent the form from submitting in form.js file

--- js/form.js
+++ js/form.js
@@ -1,15 +1,8 @@
 // Buggy form.js
 document.querySelector("form").addEventListener("submit", function(event) {
-event.preventDefault();
-
+    event.preventDefault();
     var name = document.querySelector("[name='name']").value;
     var email = document.querySelector("[name='email']").value;
-
-    // Validate Name
-    if (name === "") {
-        alert("Name is required");
-        return;
-    }
 
     // Validate Email
     if (email.indexOf('@') === -1) {
