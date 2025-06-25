97c31e76d844deba084da0d670afeff553e26c2d
Hyperez-G <jmgperez27@gmail.com>
2025-06-25 16:08:51 +0800

    Removed the click handler and handle everything in form.js file

--- js/form.js
+++ js/form.js
@@ -1,8 +1,15 @@
 // Buggy form.js
 document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
+
     var name = document.querySelector("[name='name']").value;
     var email = document.querySelector("[name='email']").value;
+
+    // Validate Name
+    if (name === "") {
+        alert("Name is required");
+        return;
+    }
 
     // Validate Email
     if (email.indexOf('@') === -1) {
@@ -10,6 +17,6 @@ document.querySelector("form").addEventListener("submit", function(event) {
         return;
     }
 
-    alert("Form submitted!");
+    alert("Form submitted!");  
     this.submit();
 });
